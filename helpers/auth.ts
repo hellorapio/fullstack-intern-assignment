2be06f0a0bbe634d62351e8f277496f56a4859da
Anubis <anubis356@outlook.com>
2024-06-28 22:20:28 +0300

    refactor: refactored code to use Next_public env

--- helpers/auth.ts
+++ helpers/auth.ts
@@ -1,11 +1,14 @@
 export async function login(body: any) {
-  const res = await fetch("http://localhost:3000/api/auth/login", {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/login",
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const data = await res.json();
 
@@ -17,13 +20,16 @@ export async function login(body: any) {
 }
 
 export async function getProtectedData(token: string) {
-  const res = await fetch("http://localhost:3000/api/protected", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-  });
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_BASE_URL + "/api/protected",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
 
   const data = await res.json();
 
